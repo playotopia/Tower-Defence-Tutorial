ae10ea66cf48b9adf8a0cd2c448f39bf77eaf415
Akram khan <46343622+Akram1871@users.noreply.github.com>
2019-03-18 00:23:50 +0600

    Script

---
+++ playotopia-practice-tower-defence/Assets/WaveSpawner.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyprefab;
+    public Transform spawnpoint;
+    public Text wavecountdownText;
+    public float timeBetweenWaves = 5f;
+    private float countdown=2f;
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+        wavecountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+    void SpawnEnemy()
+    {
+        Instantiate(enemyprefab, spawnpoint.position, spawnpoint.rotation);
+    }
+}
